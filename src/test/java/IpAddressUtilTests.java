91593643b89f5c1fc2f8db4614415a2b01e81d7d
Nikolas Laus <bchik3@gmail.com>
2021-12-05 15:28:06 +0200

    Add test for network address.

--- src/test/java/IpAddressUtilTests.java
+++ src/test/java/IpAddressUtilTests.java
@@ -3,7 +3,12 @@ import org.junit.jupiter.api.Test;
 
 public class IpAddressUtilTests {
     @Test
-    public void validateIpWithThreeOctets() {
-        Assertions.assertFalse(IpAddressUtil.validateIpv4Address("0.0.0"));
+    public void validateIpWithThreeOctetsIsInvalid() {
+        Assertions.assertFalse(IpAddressUtil.validateIpv4Address("10.0.1"));
+    }
+
+    @Test
+    public void validateNetworkAddressIsInvalid() {
+        Assertions.assertFalse(IpAddressUtil.validateIpv4Address("1.1.1.0"));
     }
 }
