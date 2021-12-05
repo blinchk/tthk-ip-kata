022a71f757e36cb238893f78c8b23f08b4d742c5
Nikolas Laus <bchik3@gmail.com>
2021-12-05 15:17:05 +0200

    Initial commit.

---
+++ src/main/java/IpAddressUtil.java
@@ -0,0 +1,10 @@
+public class IpAddressUtil {
+    private static final int MINIMUM_POSSIBLE_VALUE = 0;
+    private static final int MAXIMUM_POSSIBLE_VALUE = 255;
+    private static final int VALID_OCTETS_COUNT = 4;
+    private static final String OCTET_DELIMITER_PATTERN = ".";
+
+    public static boolean validateIpv4Address(String ip) {
+        return false;
+    }
+}
