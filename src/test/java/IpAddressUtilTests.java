247d029dde3a33da67032a6e3bc0e69f15fed1d6
Nikolas Laus <bchik3@gmail.com>
2021-12-05 15:18:30 +0200

    Add failed test.

--- src/test/java/IpAddressUtilTests.java
+++ src/test/java/IpAddressUtilTests.java
@@ -1,5 +1,9 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class IpAddressUtilTests {
-
+    @Test
+    public void validateAnyIp() {
+        Assertions.assertTrue(IpAddressUtil.validateIpv4Address("1.1.1.1"));
+    }
 }
